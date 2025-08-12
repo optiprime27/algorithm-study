97c46d06a22f874931b184f8ee76919d0807e96e
soyeoneeii <syp0463@gmail.com>
2025-08-13 00:29:40 +0900

    [refactor] 폴더 구조 변경

---
+++ 그냥풀고싶은문제/랜덤마라톤_62/boj_14581.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    cout << ":fan::fan::fan:"<<endl;
+    cout << ":fan::"<<s<<"::fan:"<<endl;
+    cout << ":fan::fan::fan:"<<endl;
+}
